31a36f22fb8e164abe51bec9619529dbb31cc050
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-05-29 13:55:54 -0700

    Added 1.1.8

--- cloudmersive_ocr_api_client/configuration.py
+++ cloudmersive_ocr_api_client/configuration.py
@@ -243,5 +243,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1\n"\
-               "SDK Package Version: 1.1.7".\
+               "SDK Package Version: 1.1.8".\
                format(env=sys.platform, pyversion=sys.version)
