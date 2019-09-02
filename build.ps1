289cf8660def2c2c26092d578bd3e9b9d6735ec2
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-09-02 11:48:15 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -5,5 +5,5 @@ Write-Host $extrasetup
 (Get-Content ./setup.py).replace('# http://pypi.python.org/pypi/setuptools', $extrasetup) | Set-Content ./setup.py
 (Get-Content ./setup.py).replace('"""\', "long_description,`n    long_description_content_type='text/markdown'") | Set-Content ./setup.py
 (Get-Content ./setup.py).replace('The powerful Optical Character Recognition (OCR) APIs let you convert scanned images of pages into recognized text.  # noqa: E501', '') | Set-Content ./setup.py
-(Get-Content ./setup.py).replace('"""', '') | Set-Content ./setup.py
+(Get-Content ./setup.py).replace('    """', '') | Set-Content ./setup.py
 Write-Host "Done."
