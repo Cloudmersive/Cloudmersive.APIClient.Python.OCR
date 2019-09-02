9addd9c9198cda591498e4e269254140c20c97cf
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-09-02 11:43:16 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 ﻿#Remove-Item –path ./ –recurse
 & java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/ocr -l python -c packageconfig.json
-$extrasetup = (Get-Content ./extrasetup.py)
+$extrasetup = (Get-Content ./extrasetup.py) -join "`n"
 Write-Host $extrasetup
 (Get-Content ./setup.py).replace('# http://pypi.python.org/pypi/setuptools', $extrasetup) | Set-Content ./setup.py
 Write-Host "Done."
--- setup.py
+++ setup.py
@@ -20,7 +20,10 @@ VERSION = "2.0.9"
 # python setup.py install
 #
 # prerequisite: setuptools
-from os import path this_directory = path.abspath(path.dirname(__file__)) with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:     long_description = f.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 REQUIRES = [
     "certifi>=2017.4.17",
