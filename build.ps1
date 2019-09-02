115a49e213bd30c9668e99b7d7bccf6472967e64
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-09-02 11:42:07 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
 ﻿#Remove-Item –path ./ –recurse
 & java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/ocr -l python -c packageconfig.json
-(Get-Content ./setup.py).replace('# http://pypi.python.org/pypi/setuptools', (Get-Content ./extrasetup.py)) | Set-Content ./setup.py
+$extrasetup = (Get-Content ./extrasetup.py)
+Write-Host $extrasetup
+(Get-Content ./setup.py).replace('# http://pypi.python.org/pypi/setuptools', $extrasetup) | Set-Content ./setup.py
+Write-Host "Done."
