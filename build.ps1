73717fad3cf6a1a347af2ac49e76bfe3d2390345
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-06-25 17:44:06 -0700

    3.0.1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-﻿#Remove-Item –path ./ –recurse
-& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/ocr -l python -c packageconfig.json
+﻿Remove-Item –path ./cloudmersive_ocr_api_client –recurse
+& java -jar swagger-codegen-cli-2.4.14.jar generate -i https://api.cloudmersive.com/swagger/api/ocr -l python -c packageconfig.json
 $extrasetup = (Get-Content ./extrasetup.py) -join "`n"
 Write-Host $extrasetup
 (Get-Content ./setup.py).replace('# http://pypi.python.org/pypi/setuptools', $extrasetup) | Set-Content ./setup.py
